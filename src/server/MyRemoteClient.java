630a5b8e32f737c992072ab13160b29b69b06392
p-artie <artiepolanco@gmail.com>
2022-05-16 17:44:30 -0400

    Build client side

--- src/server/MyRemoteClient.java
+++ src/server/MyRemoteClient.java
@@ -1,5 +1,21 @@
 package server;
 
-public class MyRemoteClient {
+import java.rmi.Naming;
 
+public class MyRemoteClient {
+	public static void main(String[] args) {
+		new MyRemoteClient().go();
+	}
+	
+	public  void go() {
+		try {
+			MyRemote service = (MyRemote) Naming.lookup("rmi://127.0.0.1/Remote Hello");
+			
+			String s = service.sayHello();
+			
+			System.out.println(s);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 }
